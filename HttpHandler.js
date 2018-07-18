6de049698838cccafb1ef842b76d2eeae71d9312
teofpqy <teofpqy@outlook.com>
2018-07-18 10:31:27 +0800

    add proxy url in config.

--- HttpHandler.js
+++ HttpHandler.js
@@ -1,9 +1,11 @@
 const axios = require('axios');
+const config = require('./config');
+
 const api = axios.create({
-    baseURL: 'https://csgo.5eplay.com/',
+    baseURL: config.proxyBaseUrl,
     timeout: 1000,
-    headers: {'X-Custom-Header': 'foobar'}
-})
+});
+
 const httpHandler = async (ctx) => {
     const requestUrl = ctx.request.url;
     const response = await api.get(requestUrl);
--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const CONF = {
   port: 8000,
+  proxyBaseUrl: 'https://csgo.5eplay.com/',
 }
 
 module.exports = CONF;
